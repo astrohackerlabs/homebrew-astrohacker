90da4ce21a389f34d7eb79adcea1647ebe85f562
Ryan X. Charles <ryan@ryanxcharles.com>
2026-09-19 08:31:42 -0500

    Release RedVim 0.7.3

--- Formula/redvim.rb
+++ Formula/redvim.rb
@@ -1,9 +1,9 @@
 class Redvim < Formula
   desc "Astrohacker modal editor with built-in Nushell highlighting"
   homepage "https://github.com/astrohackerlabs/redvim"
-  url "https://github.com/astrohackerlabs/redvim/releases/download/v0.7.2/redvim-0.7.2-aarch64-apple-darwin.tar.gz"
-  version "0.7.2"
-  sha256 "bdb180d9d2f4a17f07d041527582c1204eceab30217ae10ee1aa8ed79b4013c8"
+  url "https://github.com/astrohackerlabs/redvim/releases/download/v0.7.3/redvim-0.7.3-aarch64-apple-darwin.tar.gz"
+  version "0.7.3"
+  sha256 "0098fa1e6aebdee28d0426edd50dca2f7bddf1895d4b30dc151b7d903c6a1b14"
   license all_of: ["MIT", "Apache-2.0"]
 
   depends_on arch: :arm64
@@ -29,6 +29,9 @@ class Redvim < Formula
     config.unlink
     output = shell_output("#{bin}/redvim --self-check")
     assert_match "language nu: bundled highlighting ok", output
+    %w[c cpp python html css ruby zig swift xml make].each do |language|
+      assert_match "language #{language}: bundled highlighting ok", output
+    end
     assert_match "redvim self-check ok", output
   end
 end
