b6abaad810d4bddc4b8e5b6ded59244884a68318
Ryan X. Charles <ryan@ryanxcharles.com>
2026-09-19 16:32:52 -0500

    Release Roamari 0.1.1

--- Formula/roamari.rb
+++ Formula/roamari.rb
@@ -1,9 +1,9 @@
 class Roamari < Formula
   desc "Terminal browser client for Astrohacker TermSurf"
   homepage "https://github.com/astrohackerlabs/roamari"
-  url "https://github.com/astrohackerlabs/roamari/releases/download/v0.1.0/roamari-0.1.0-aarch64-apple-darwin.tar.gz"
-  version "0.1.0"
-  sha256 "47de4b5468b739ba1a01c1a6a47ea3ff3e04f31434565a8836ba5877e083ff8d"
+  url "https://github.com/astrohackerlabs/roamari/releases/download/v0.1.1/roamari-0.1.1-aarch64-apple-darwin.tar.gz"
+  version "0.1.1"
+  sha256 "dfada32ac16ea759279115601ec83a509e73974300d79a4b45deb086166e08c7"
   license "MIT"
 
   depends_on arch: :arm64
