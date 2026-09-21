01e1d2d3aa5830a5bc43691f5ebee150f26cfb15
Ryan X. Charles <ryan@ryanxcharles.com>
2026-09-21 07:07:21 -0500

    Release NuTorch 2.0.9 binary

--- Formula/nutorch.rb
+++ Formula/nutorch.rb
@@ -1,9 +1,9 @@
 class Nutorch < Formula
   desc "Nushell-based shell with built-in GPU tensors and neural networks"
   homepage "https://github.com/astrohackerlabs/nutorch"
-  url "https://github.com/astrohackerlabs/nutorch/releases/download/v2.0.8/nutorch-2.0.8-arm64-tahoe.tar.gz"
-  version "2.0.8"
-  sha256 "062d8341c790841abfbcf0801b6ef6648d875a8bd40bba692d0f59dce8fb7b83"
+  url "https://github.com/astrohackerlabs/nutorch/releases/download/v2.0.9/nutorch-2.0.9-arm64-tahoe.tar.gz"
+  version "2.0.9"
+  sha256 "bb9edcac7137f14c57ec8223485e9eae97c78e227444235b635aa79451dea859"
   license "MIT"
 
   depends_on arch: :arm64
