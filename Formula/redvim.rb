c64c1aab148661fdb02d50038d860a99f8a73f8b
Ryan X. Charles <ryan@ryanxcharles.com>
2026-09-26 05:09:25 -0500

    Release RedVim 0.7.9

--- Formula/redvim.rb
+++ Formula/redvim.rb
@@ -1,9 +1,9 @@
 class Redvim < Formula
   desc "Astrohacker modal editor with built-in Nushell highlighting"
   homepage "https://github.com/astrohackerlabs/redvim"
-  url "https://github.com/astrohackerlabs/redvim/releases/download/v0.7.8/redvim-0.7.8-aarch64-apple-darwin.tar.gz"
-  version "0.7.8"
-  sha256 "3c9a54d29101df1a6f32547c9467f1221dd35ef7a57b175507447e54ef5a5c98"
+  url "https://github.com/astrohackerlabs/redvim/releases/download/v0.7.9/redvim-0.7.9-aarch64-apple-darwin.tar.gz"
+  version "0.7.9"
+  sha256 "870317ac16ea68202df9ec4d60f38ee7628e7f57d746e333495b711423a986f5"
   license all_of: ["MIT", "Apache-2.0", "W3C-20150513", "BSD-3-Clause"]
 
   depends_on arch: :arm64
@@ -29,7 +29,7 @@ class Redvim < Formula
     config.unlink
     output = shell_output("#{bin}/redvim --self-check")
     assert_match "language nu: bundled highlighting ok", output
-    %w[c cpp python html css ruby zig swift xml make sql hcl objc proto dockerfile gn wgsl caddyfile applescript].each do |language|
+    %w[c cpp python html css ruby zig swift xml make sql hcl objc proto dockerfile gn wgsl caddyfile applescript typst].each do |language|
       assert_match "language #{language}: bundled highlighting ok", output
     end
     assert_match "redvim self-check ok", output
